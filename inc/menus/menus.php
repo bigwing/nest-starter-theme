7076e6d9494c59e304d98a827c128bf3d92a814a
Tia Nguyen <tnguyen@tnguyenmb.opubco.oklahoman.com>
2015-12-22 14:03:54 -0600

    Fix register social nav.

--- inc/menus/menus.php
+++ inc/menus/menus.php
@@ -3,7 +3,7 @@ class nest_Menus {
     public function __construct() {
         add_action( 'init', function() {
             register_nav_menus(array( // Using array to specify more menus if needed
-            	'social-menu' => 'Social Menu',
+            	'social-nav' => 'Social Menu',
             	'header-top-utility-nav' => 'Header Top Menu',
                 'header-nav' => 'Header Main Menu',
                 'footer-nav' => 'Footer Menu' // Footer Navigation
